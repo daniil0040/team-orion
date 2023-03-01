9f3214a46d3dbbbc6c9b8dca2e9751369b393bdb
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-03-01 10:31:33 +0100

    Add restaurant-menu slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,20 +1,24 @@
 $('.responsive').slick({
-  // prevArrow: '<button type="button" class="my-slick-prev"></button>',
-  // nextArrow: '<button type="button" class="my-slick-next"></button>',
-  prevArrow: '<button type="button" class="my-slick-prev">&#8249;</button>',
-  nextArrow: '<button type="button" class="my-slick-next">&#8250;</button>',
+  prevArrow: '<button type="button" class="my-slick-prev"></button>',
+  nextArrow: '<button type="button" class="my-slick-next"></button>',
   infinite: true,
   speed: 400,
   slidesToScroll: 1,
-  adaptiveHeight: true,
+  adaptiveHeight: false,
   slidesToShow: 1,
   variableWidth: false,
+  infinite: false,
+  waitForAnimate: false,
+  fade: true,
   responsive: [
     {
+      breakpoint: 1400,
+      settings: 'unslick',
+    },
+    {
       breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-      },
+      settings: 'slick',
     },
   ],
+  mobileFirst: true,
 });
